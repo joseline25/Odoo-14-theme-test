4e2399849959ef75d49edbee86b801c03ff37ab3
joseline <youjoseline@gmail.com>
2021-07-29 12:26:51 +0100

    la version finale avec les commentaires des menus

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'les pages du 237',
+    'description': 'Tutoriel de création de thème',
+    'version': '1.1',
+    'author': 'Joseline et Gaetan',
+    'category': 'Theme/Creative',
+
+    'depends': ['website'],
+    'data': ['views/layout.xml', 'views/pages.xml', 'views/options.xml', 'views/snippets.xml'
+    ],
+    'installable': True,
+}
